cffe47cb7354010c630f83034415029f84af209e
Diego Gallardo <diego.gallardo@wpengine.com>
2023-02-18 18:54:25 -0600

    initial commit

---
+++ chewyscript.js
@@ -0,0 +1,4 @@
+export default function unique(array) {
+  let uniqueArray = new Set(array)
+  return uniqueArray
+}
---
+++ test.js
@@ -0,0 +1,3 @@
+import unique from "./chewyscript";
+
+console.log(unique([0, 0, 1, 2, 3, 4, 4, 5, 6, 6, 6]))
